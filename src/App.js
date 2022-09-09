8257e3306e19c5982b26371d1214334ba28904a0
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-09 15:26:07 -0400

    Updated 'More info' button to use disclosure, added hyperlinks to urls

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useState } from "react"
 import { books } from "./books"
+import styles from "./App.css"
 
 export const App = () => {
-    
-
     return(
         <>
-            <h1>Text Display</h1>
+            <h1>Books</h1>
             <div className="book-list">
                 {books.map((book, index) => (
                     <Book title={book.title} author={book.author} url={book.url} shortDescription={book.shortDescription} coverImageUrl={book.coverImageUrl} publisher={book.publisher} publicationDate={book.publicationDate} detailedDescription={book.detailedDescription} 
@@ -26,14 +25,14 @@ const Book = ({ title, author, url, shortDescription, coverImageUrl, publisher, 
 
     return(
         <div style={{ border: '1px solid purple', margin: '10px' }}>
-            <p> {title} </p>
+            <p className='title'> {title} </p>
             <p> {author} </p>
             <p> {shortDescription} </p>
             <p> {coverImageUrl} </p>
-            <button onClick={() => handleClick()}>
+            <button onClick={() => handleClick()} aria-expanded={ expanded }>
                 {expanded ? 'Less' : 'More'} info
             </button>
-            {expanded ? <p>URL: {url}</p> : ''}
+            {expanded ? <p>URL: <a href={url}>{url}</a></p> : ''}
             {expanded ? <p>Publisher: {publisher}</p> : ''}
             {expanded ? <p>Publication Date: {publicationDate}</p> : ''}
             {expanded ? <p>Full Description: {detailedDescription}</p> : ''}
