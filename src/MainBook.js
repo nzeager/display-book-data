6944d70a2d15c6e7c584ffca599a6ac723877346
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-11 19:33:28 -0400

    Broken image links get replaced with default image

--- src/MainBook.js
+++ src/MainBook.js
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 export const MainBook = ({title, author, shortDescription, coverImageUrl}) => {
+
+    const [imageBroken, setImageBroken] = useState(false)
+
     return(
         <div className="mainInfo">
             <div>
@@ -7,7 +12,7 @@ export const MainBook = ({title, author, shortDescription, coverImageUrl}) => {
                 { shortDescription ? <p> {shortDescription} </p> : "" }
             </div>
             <div>
-                { coverImageUrl ? <p className='cover'> <img src={coverImageUrl} alt=""></img> </p> : "" }
+                { coverImageUrl ? <p className='cover'> <img src={imageBroken ? 'https://cdn-icons-png.flaticon.com/512/107/107817.png' : coverImageUrl} onError={() => setImageBroken(true)} alt={"Cover for " + title}></img> </p> : "" }
             </div>
         </div>
     )
